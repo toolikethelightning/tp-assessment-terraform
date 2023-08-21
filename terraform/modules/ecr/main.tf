eca518eff3bbcfb94af27bf2234325a1974bf41e
toolikethelightning <toolikethelightning@protonmail.ch>
2023-08-21 18:40:31 +0100

    Remove ECR Repo tags (as it seems to cause a permissions errors)

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "hello_app_ecr_repo" {
   name = "hello-app-ecr-repo"
-  tags = {
-    Name = "hello-app-ecr-repo"
-    Project = "hello-app"
-  }
+#   tags = {
+#     Name = "hello-app-ecr-repo"
+#     Project = "hello-app"
+#   }
 }
